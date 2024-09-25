7ea84adf9a32e1e4a3232e79becbe9ab9596b63a
SUN DORIS LI-YING <dorisyukisun@gmail.com>
2024-09-25 15:47:27 +0800

    Springboot_lswebiste01

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.loginform.LoginForm;
+
+@Controller
+@RequestMapping
+public class LoginController {
+	@GetMapping("/test")
+	public String testPage() {
+		return "test";
+	}
+	
+	  // 定義GET請求的路徑 /lswebsite/login
+	@GetMapping("/admin/manage-users/login")
+	public String login(Model model) {
+		LoginForm loginForm = new LoginForm();
+		model.addAttribute("LoginForm",loginForm); //Add form-backing object
+		System.out.println("Loginform added to model: " + loginForm);
+		
+		return "admin/login";
+		// 返回 login.html 模板頁面
+	}
+	
+
+}
